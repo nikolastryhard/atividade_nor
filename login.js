6631c32250382f246bf6100991fa8dee3a90a551
Nikolas rafael <galvao.nikolas@escola.pr.gov.br>
2026-06-16 08:49:43 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+const loginform = document.querySelector('#login-form');
+const email = document.querySelector('#email');
+const passwordinput = document.querySelector('#password');
+const messagediv = document.querySelector('#messagediv');
+
+function primeirafuncao(){
+    console.log( loginform)
+    console.log( email)
+    console.log( passwordinput)
+    console.log( messagediv)
+}
+
+function segundafuncao(){
+
+}
